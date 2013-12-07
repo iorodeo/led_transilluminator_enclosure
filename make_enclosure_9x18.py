ec49fc666403ad0dfd3fcdf067224132da367350
Jo Long <jo@iorodeo.com>
2013-12-06 16:39:48 -0800

    Initial designs for the 9x18 led transilluminator enclosure.

--- make_enclosure_9x18.py
+++ make_enclosure_9x18.py
@@ -9,14 +9,14 @@ from led_trans_enclosure import LED_Trans_Enclosure
 
 
 INCH2MM = 25.4
-PROJECTION =True 
-MAKE_DXF = True 
-model = '8x9'
+PROJECTION = True
+MAKE_DXF = True
+model = '9x18'
 
 params = {
         'inner_height'            : 1.2505*INCH2MM,
-        'pcb_mount_hole_dx'       : 4.1953*INCH2MM,
-        'pcb_mount_hole_dy'       : 2.1894*INCH2MM,
+        'pcb_mount_hole_dx'       : 4.0949*INCH2MM,
+        'pcb_mount_hole_dy'       : 5.3543*INCH2MM,
         'pcb_mount_hole_diam'     : 0.1160*INCH2MM,
         'wall_thickness'          : 3.0, 
         'top_upper_thickness'     : 1.5,
@@ -28,7 +28,7 @@ params = {
         'bottom_x_overhang'       : 0.25*INCH2MM,
         'bottom_y_overhang'       : 0.08*INCH2MM, 
         'lid2front_tabs'          : (0.2,0.5,0.8),
-        'lid2side_tabs'           : (0.25, 0.75),
+        'lid2side_tabs'           : (0.2,0.5,0.8),
         'side2side_tabs'          : (0.5,),
         'lid2front_tab_width'     : 0.5*INCH2MM,
         'lid2side_tab_width'      : 0.5*INCH2MM, 
@@ -37,20 +37,20 @@ params = {
         'tab_depth_bot'           : 3.0,
         'tab_depth_side'          : 3.0,
         'standoff_diameter'       : 0.25*INCH2MM,
-        'standoff_offset'         : 0.05*INCH2MM, 
+        'standoff_offset'         : 0.10*INCH2MM, 
         'standoff_hole_diameter'  : 0.116*INCH2MM,   
         'pcb_standoff_height'     : (3.0/16.0)*INCH2MM,
         'pcb_thickness'           : 0.061*INCH2MM,
         'dc_jack_height'          : 0.25*INCH2MM,
-        'dc_jack_offset_x'        : -0.415*INCH2MM,
+        'dc_jack_offset_x'        : 1.8898*INCH2MM,
         'switch_height'           : 7.2, 
-        'switch_offset_x'         : 0.335*INCH2MM, 
+        'switch_offset_x'         : 0.7087*INCH2MM, 
         'switch_hole_x'           : 0.40*INCH2MM,
         'switch_hole_y'           : 6.0,
         'switch_hole_radius'      : 1.0,
         'dc_plug_diam'            : 0.38*INCH2MM,
-        'led_cutout_dx'           : 2.718*INCH2MM,
-        'led_cutout_dy'           : 2.41*INCH2MM,
+        'led_cutout_dx'           : 70,
+        'led_cutout_dy'           : 140,
         'vent_hole_diameter'      : 0.15*INCH2MM,
         'vent_hole_array_dx'      : 2.718*INCH2MM,
         'vent_hole_array_dy'      : 2.41*INCH2MM,
@@ -67,7 +67,7 @@ enclosure.make()
 
 part_assembly = enclosure.get_assembly(
         explode=(0,0,10),
-        show_top_upper = True, 
+        show_top_upper = False, 
         show_top_middle = True,
         show_bottom = True,
         show_left =  True,
